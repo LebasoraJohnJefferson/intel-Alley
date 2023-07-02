7e79a5fd92752f109d7af036ef5dc3d26922a0e2
kate <christiannparanas@gmail.com>
2023-07-03 00:44:15 +0800

    folder structure

---
+++ src/app/modules/alumnus/alumnus.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AlumnusRoutingModule } from './alumnus-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AlumnusRoutingModule
+  ]
+})
+export class AlumnusModule { }
