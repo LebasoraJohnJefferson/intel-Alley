a2086328b4a782730d034668f11f23ef35c67b82
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2023-07-04 17:11:10 +0800

    adding 404 page

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -4,11 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { ForgotpasswordComponent } from './pages/forgotpassword/forgotpassword.component';
 import { ResetpasswordComponent } from './pages/resetpassword/resetpassword.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'forgotpassword', component: ForgotpasswordComponent },
   { path: 'resetpassword', component: ResetpasswordComponent },
+  {path:'page-not-found',component:PageNotFoundComponent},
+  { path:"**", redirectTo:'page-not-found'}
 ];
 
 @NgModule({
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,13 +5,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { PrimengModule } from '../modules/primeng/primeng.module';
 
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 import { CoreRoutingModule } from './core-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { ResetpasswordComponent } from './pages/resetpassword/resetpassword.component';
 import { ForgotpasswordComponent } from './pages/forgotpassword/forgotpassword.component';
 import { CustomBtnComponent } from './components/custom-btn/custom-btn.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 
 @NgModule({
@@ -20,6 +22,7 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
     ResetpasswordComponent,
     ForgotpasswordComponent,
     CustomBtnComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     CommonModule,
