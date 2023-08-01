4b408f0315cb75cb82094c05a06b7c0895fe5675
Christian Paranas <dev.christianparanas@gmail.com>
2023-08-02 00:22:01 +0800

    chages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,22 @@
 export const environment = {
   production: true,
-  baseURL: "https://intel-alley-backend-production.up.railway.app"
+  baseURL: 'https://intel-alley-backend-production.up.railway.app',
 };
 
 export const developersProfile = {
-  developerOne:{
-    name:'Alexis Luke Barlomento',
-    profile:'https://scontent.fmnl8-1.fna.fbcdn.net/v/t39.30808-6/357093634_1565851087553709_6622943989935726886_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=9ZKXUHvFrBAAX-Dv1bG&_nc_ht=scontent.fmnl8-1.fna&oh=00_AfANdEjCj_VZzyXMTn5ATMOfaDMGrcrHzsLtLfS70VhTXA&oe=64B43E30'
+  developerOne: {
+    name: 'Alexis Luke Barlomento',
+    profile:
+      'https://scontent.fmnl8-1.fna.fbcdn.net/v/t39.30808-6/357093634_1565851087553709_6622943989935726886_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=9ZKXUHvFrBAAX-Dv1bG&_nc_ht=scontent.fmnl8-1.fna&oh=00_AfANdEjCj_VZzyXMTn5ATMOfaDMGrcrHzsLtLfS70VhTXA&oe=64B43E30',
   },
-  developerTwo:{
-    name:'Anjelo Villafranca',
-    profile:'https://cdn-icons-png.flaticon.com/512/2748/2748558.png'
+  developerTwo: {
+    name: 'Anjelo Villafranca',
+    profile:
+      'https://scontent.fmnl8-1.fna.fbcdn.net/v/t39.30808-6/357093634_1565851087553709_6622943989935726886_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=9ZKXUHvFrBAAX-Dv1bG&_nc_ht=scontent.fmnl8-1.fna&oh=00_AfANdEjCj_VZzyXMTn5ATMOfaDMGrcrHzsLtLfS70VhTXA&oe=64B43E30',
   },
-  developerThree:{
-    name:'Marlone Adalin',
-    profile:'https://scontent.fmnl8-1.fna.fbcdn.net/v/t39.30808-6/344569958_160160767011446_8090380351600914881_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=174925&_nc_ohc=3aI3_nlXmiEAX_ogpQm&_nc_ht=scontent.fmnl8-1.fna&oh=00_AfDKVNQzlXPQobaBB46Lf7hv6URqOpVnbmNVUXoL2XqU5w&oe=64B46B02'
-  }
-}
+  developerThree: {
+    name: 'Marlone Adalin',
+    profile:
+      'https://scontent.fmnl8-1.fna.fbcdn.net/v/t39.30808-6/344569958_160160767011446_8090380351600914881_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=174925&_nc_ohc=3aI3_nlXmiEAX_ogpQm&_nc_ht=scontent.fmnl8-1.fna&oh=00_AfDKVNQzlXPQobaBB46Lf7hv6URqOpVnbmNVUXoL2XqU5w&oe=64B46B02',
+  },
+};
