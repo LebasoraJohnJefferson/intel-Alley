aaeb4cc5d4832efc399a25966c89bcc5a067447e
Christian Paranas <dev.christianparanas@gmail.com>
2023-08-07 11:00:54 +0800

    interface modifications

--- src/app/core/pages/forgotpassword/forgotpassword.component.ts
+++ src/app/core/pages/forgotpassword/forgotpassword.component.ts
@@ -1,52 +1,52 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
-import { FormBuilder,Validators,FormGroup } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { HotToastService } from '@ngneat/hot-toast';
 import { AuthService } from '../../shared/services/auth.service';
-
 
 @Component({
   selector: 'app-forgotpassword',
   templateUrl: './forgotpassword.component.html',
-  styleUrls: ['./forgotpassword.component.scss']
+  styleUrls: ['./forgotpassword.component.scss'],
 })
 export class ForgotpasswordComponent {
-  isLoading:boolean = false
-  token:string = ''
+  submitLoading: boolean = false;
+  token: string = '';
+  isEmailSent: boolean = false;
 
-  emailForm:FormGroup = this._fb.group({
-    email:['',[Validators.required,Validators.email]]
-  })
-
-
+  emailForm: FormGroup = this._fb.group({
+    email: ['', [Validators.required, Validators.email]],
+  });
 
   constructor(
-    public location:Location,
-    private _fb:FormBuilder,
-    public toast:HotToastService,
-    private _authService:AuthService
-    ){
-      
+    public location: Location,
+    private _fb: FormBuilder,
+    public toast: HotToastService,
+    private _authService: AuthService
+  ) {}
+
+  goBack() {
+    this.location.back();
   }
 
-  goBack(){
-    this.location.back()
-  }
+  onSubmit() {
+    if (this.emailForm.valid) {
+      this._authService.forgotpassword(this.emailForm.value).subscribe(
+        (res: any) => {
+          this.toast.success(res.message);
+          this.emailForm.reset();
+          this.submitLoading = false;
 
-  clickSubmit(event:boolean){
-    this.isLoading = !event
-    if(this.emailForm.valid){
-      this._authService.forgotpassword(this.emailForm.value).subscribe((res:any)=>{
-        this.toast.success(res.message)
-        this.emailForm.reset()
-        this.isLoading = false
-      },(err:any)=>{
-        this.toast.warning(err.error)
-        this.isLoading = false
-      })
-    }else{
-      this.toast.warning('Email is required!')
-      this.isLoading = false
+          this.isEmailSent = true;
+        },
+        (err: any) => {
+          this.toast.warning(err.error);
+          this.submitLoading = false;
+        }
+      );
+    } else {
+      this.toast.warning('Email is required!');
+      this.submitLoading = false;
     }
   }
 }
--- src/app/core/pages/resetpassword/resetpassword.component.ts
+++ src/app/core/pages/resetpassword/resetpassword.component.ts
@@ -1,63 +1,66 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup,Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HotToastService } from '@ngneat/hot-toast';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../shared/services/auth.service';
 
-
 @Component({
   selector: 'app-resetpassword',
   templateUrl: './resetpassword.component.html',
-  styleUrls: ['./resetpassword.component.scss']
+  styleUrls: ['./resetpassword.component.scss'],
 })
 export class ResetpasswordComponent {
-  isLoading:boolean =false
-  token:any;
-  passwordForm:FormGroup = this._fb.group({
-    password:['',Validators.required],
-    confirmPassword:['',Validators.required],
-  })
+  submitLoading: boolean = false;
+  token: any;
+
+  passwordForm: FormGroup = this._fb.group({
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+  });
 
   constructor(
-    private _fb:FormBuilder,
-    public toast:HotToastService,
-    private _route:ActivatedRoute,
-    private _authService:AuthService,
-    public router:Router
-  ){
+    private _fb: FormBuilder,
+    public toast: HotToastService,
+    private _route: ActivatedRoute,
+    private _authService: AuthService,
+    public router: Router
+  ) {
     this._route.queryParams.subscribe((value) => {
       this.token = this._route.snapshot.paramMap.get('token');
     });
   }
 
-  goBack(){
-    this.router.navigate(['/login'])
+  goBack() {
+    this.router.navigate(['/login']);
   }
 
-
-  clickSubmit(event:boolean){
-    this.isLoading = !event
-    if(this.passwordForm.invalid){
-      this.toast.warning('Empty Input(s)')
-      this.isLoading = false
-    }else if(this.passwordForm.controls['password'].value !=this.passwordForm.controls['confirmPassword'].value){
-      this.toast.warning('password does`nt match')
-      this.isLoading = false
-    }else{
-      this._authService.resetPassword(this.passwordForm.value,this.token).subscribe({
-        complete:()=>{
-          this.isLoading = false
-          this.passwordForm.reset()
-        },
-        error:(err)=>{
-          this.toast.warning(err.error)
-          this.isLoading = false
-        },next:(res)=>{
-          this.toast.success(res.message)
-          this.router.navigate(['/login'])
-        }
-      })
+  onSubmit() {
+    if (this.passwordForm.invalid) {
+      this.toast.warning('Empty Input(s)');
+      this.submitLoading = false;
+    } else if (
+      this.passwordForm.controls['password'].value !=
+      this.passwordForm.controls['confirmPassword'].value
+    ) {
+      this.toast.error("Password didn't matched.");
+      this.submitLoading = false;
+    } else {
+      this._authService
+        .resetPassword(this.passwordForm.value, this.token)
+        .subscribe({
+          complete: () => {
+            this.submitLoading = false;
+            this.passwordForm.reset();
+          },
+          error: (err) => {
+            this.toast.warning(err.error);
+            this.submitLoading = false;
+          },
+          next: (res) => {
+            this.toast.success(res.message);
+            this.router.navigate(['/login']);
+          },
+        });
     }
   }
 }
-
