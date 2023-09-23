deeab2a7c9672bdd135b93d9cf60cdafcddb8f07
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2023-09-24 02:38:36 +0800

    change image default in header

--- src/app/modules/admin/components/header/header.component.ts
+++ src/app/modules/admin/components/header/header.component.ts
@@ -14,7 +14,7 @@ export class HeaderComponent implements OnInit {
   currentRoute: any;
   onScroll: boolean = false;
   user: any = [];
-  defaultImg: any = '../../../../../assets/images/student.png';
+  defaultImg: any = '../../../../../assets/images/admin.png';
 
   notifications: any = [];
 
