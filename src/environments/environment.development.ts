3b6ef2cbffccaec4a75c93dcb1d7ba4e00e74fa8
john jefferson lebasora <johnjefferson.lebasora@evsu.edu.ph>
2023-12-04 19:35:22 +0800

    anjelo and marlone profile pic

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -7,16 +7,16 @@ export const developersProfile = {
   developerOne: {
     name: 'Alexis Luke Barlomento',
     profile:
-      'https://scontent.fmnl8-1.fna.fbcdn.net/v/t39.30808-6/357093634_1565851087553709_6622943989935726886_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=9ZKXUHvFrBAAX-Dv1bG&_nc_ht=scontent.fmnl8-1.fna&oh=00_AfANdEjCj_VZzyXMTn5ATMOfaDMGrcrHzsLtLfS70VhTXA&oe=64B43E30',
+      "../../../../../assets/developers/alex.png",
   },
   developerTwo: {
     name: 'Anjelo Villafranca',
     profile:
-      'https://scontent.fmnl8-1.fna.fbcdn.net/v/t39.30808-6/357093634_1565851087553709_6622943989935726886_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=9ZKXUHvFrBAAX-Dv1bG&_nc_ht=scontent.fmnl8-1.fna&oh=00_AfANdEjCj_VZzyXMTn5ATMOfaDMGrcrHzsLtLfS70VhTXA&oe=64B43E30',
+      '../../../../../assets/developres/anjelo.png',
   },
   developerThree: {
     name: 'Marlone Adalin',
     profile:
-      'https://scontent.fmnl8-1.fna.fbcdn.net/v/t39.30808-6/344569958_160160767011446_8090380351600914881_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=174925&_nc_ohc=3aI3_nlXmiEAX_ogpQm&_nc_ht=scontent.fmnl8-1.fna&oh=00_AfDKVNQzlXPQobaBB46Lf7hv6URqOpVnbmNVUXoL2XqU5w&oe=64B46B02',
+      '../../../../../assets/developers/marlone.png',
   },
 };
